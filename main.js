a897cf0ff1803f8fb508591f62d729516f957443
SanneJacobsson <sanne.jacobsson@gmail.com>
2023-10-20 15:47:21 +0200

    Grunder för todo list

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 import "./style.css";
+
+const userInput = document.getElementById("userInput").value;
+
+const todo = ["Tvätta", "Dammsuga", "Diska"];
+
+function createHtml(todo) {
+  const todoList = document.getElementById("todo");
+  todoList.innerHTML = "";
+
+  for (let i = 0; i < todo.length; i++) {
+    const listItem = document.createElement("div");
+    listItem.id = "listItem";
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.id = "task";
+    const pTag = document.createElement("p");
+    const removeButton = document.createElement("button");
+
+    pTag.innerHTML = todo[i];
+    removeButton.innerHTML = "Remove";
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(pTag);
+    listItem.appendChild(removeButton);
+    todoList.appendChild(listItem);
+  }
+}
+
+createHtml(todo);
