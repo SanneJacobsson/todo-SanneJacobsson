6d840f3052218462cc0c02ca8c2837ae981bf210
SanneJacobsson <sanne.jacobsson@gmail.com>
2023-10-23 15:51:30 +0200

    you can add new tasks. starting to look at moving tasks

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 import "./style.css";
 
-const userInput = document.getElementById("userInput").value;
-
 class Task {
   task;
-  isRemoved;
+  isChecked;
 
   constructor(task) {
     this.task = task;
-    this.isRemoved = false;
+    this.isChecked = false;
+  }
+}
+
+class TaskDone {
+  taskDone;
+
+  constructor(taskDone) {
+    this.taskdone = taskDone;
   }
 }
 
@@ -17,6 +23,18 @@ const t2 = new Task("Dammsuga");
 const t3 = new Task("Diska");
 
 let todo = [t1, t2, t3];
+let taskDone = [];
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", addToList);
+
+function addToList(e) {
+  e.preventDefault();
+  const userInput = document.getElementById("userInput").value;
+  const newTask = new Task(userInput);
+  todo.push(newTask);
+  createHtml(todo);
+}
 
 function createHtml(todo) {
   const todoList = document.getElementById("todo");
@@ -37,11 +55,45 @@ function createHtml(todo) {
       deleteTask(i);
     });
 
+    checkBox.addEventListener("change", () => {
+      moveTask(i);
+    });
+
     listItem.appendChild(checkBox);
     listItem.appendChild(pTag);
     listItem.appendChild(removeButton);
     todoList.appendChild(listItem);
   }
+}
+
+function createHtmlDone(taskDone) {
+  const taskDoneList = document.getElementById("taskDone");
+  //   taskDoneList.innerHTML = "";
+
+  for (let i = 0; i < taskDone.length; i++) {
+    const listDoneItem = document.createElement("div");
+    listDoneItem.className = "listItem";
+    const checkBoxDone = document.createElement("input");
+    checkBoxDone.setAttribute("type", "checkbox");
+    const pTagDone = document.createElement("p");
+    const removeButtonDone = document.createElement("button");
+
+    pTagDone.innerHTML = taskDone[i].task;
+    removeButtonDone.innerHTML = "Remove";
+
+    listDoneItem.appendChild(checkBoxDone);
+    listDoneItem.appendChild(pTagDone);
+    listDoneItem.appendChild(removeButtonDone);
+    taskDoneList.appendChild(listDoneItem);
+  }
+}
+
+function moveTask(i) {
+  todo.splice(i, 1);
+  createHtml(todo);
+  let done = new TaskDone(todo[i].task);
+  taskDone.push(done);
+  createHtmlDone(taskDone);
 }
 
 function deleteTask(i) {
